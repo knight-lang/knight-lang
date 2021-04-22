b3c1e5d5ef756e14053ec16b758c49b0816f08c7
Sam Westerman <sam.westerman@usaepay.com>
2021-04-21 18:24:09 -0700

    added OUTPUT

--- test/unit/4-function/4.2-unary/4.2.10-output.rb
+++ test/unit/4-function/4.2-unary/4.2.10-output.rb
@@ -1,8 +1,45 @@
-require_relative '../function-spec'
-
 section '4.2.10', 'OUTPUT' do
-	include Kn::Test::Spec
-	# TODO
+	before do
+		def assert_output(expected, expr)
+			assert_equal expected, exec(expr)
+		end
+	end
 
-	# #test_argument_count 'OUTPUT', '"hi"'
+	it 'just prints a newline with no string' do
+		assert_output "\n", %|OUTPUT ""|
+	end
+
+	it 'prints normally' do
+		assert_output "1\n", %|OUTPUT "1"|
+		assert_output "hello world\n", %|OUTPUT "hello world"|
+	end
+
+	it 'prints newlines correctly' do
+		assert_output "foobar\nbaz\n", %|OUTPUT "foobar\nbaz"|
+		assert_output "foobar\nbaz\n\n", %|OUTPUT "foobar\nbaz\n"|
+	end
+
+	it 'wont print with a trailing `\`' do
+		assert_output "", %|OUTPUT "\\"|
+		assert_output "hello", %|OUTPUT "hello\\"|
+		assert_output "world\n", %|OUTPUT "world\n\\"|
+	end
+
+	it 'converts values to a string' do
+		assert_output "123\n", %|OUTPUT 123|
+		assert_output "-123\n", %|OUTPUT - 0 123|
+		assert_output "true\n", %|OUTPUT TRUE|
+		assert_output "false\n", %|OUTPUT FALSE|
+		assert_output "null\n", %|OUTPUT NULL|
+	end
+
+	it 'does not allow blocks or variables as the first operand', when_testing: :strict_types do
+		refute_runs %|; = a "3" : OUTPUT BLOCK a|
+		refute_runs %|OUTPUT BLOCK QUIT 0|
+	end
+
+	it 'requires exactly one argument', when_testing: :argument_count do
+		refute_runs %|OUTPUT|
+		assert_runs { exec %|OUTPUT "1"| }
+	end
 end
